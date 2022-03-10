baa8dcd83ce377b58b30160ba75203b8f2d500f7
jokerwen666 <316295337@qq.com>
2022-03-10 13:59:39 +0800

    2022.3.10测试版本更新

--- src/main/java/com/hust/nodecontroller/utils/IndustryQueryUtil.java
+++ src/main/java/com/hust/nodecontroller/utils/IndustryQueryUtil.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.Semaphore;
 
 public class IndustryQueryUtil {
